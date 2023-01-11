89123b0db7fb202c4d85445d1a2cb855c547d8e7
KishorDMane <manekishor212@gmail.com>
2023-01-11 09:49:33 +0530

    resume

---
+++ src/nav.js
@@ -0,0 +1,5 @@
+// const button_resume=document.getElementById("Resume_btn")
+// button_resume.addEventListener("click",()=>{
+//     window.location.href="https://drive.google.com/file/d/1vCoAWwfoIguQHzeeyadGaWNapsln_fDv/view?usp=share_link"
+//     window.location.href="https://drive.google.com/u/0/uc?id=1vCoAWwfoIguQHzeeyadGaWNapsln_fDv&export=download"
+// })
---
+++ src/resume.js
@@ -0,0 +1,5 @@
+let btn=document.getElementById("button")
+btn.addEventListener("click",()=>{
+    window.location.href="https://drive.google.com/u/0/uc?id=1vCoAWwfoIguQHzeeyadGaWNapsln_fDv&export=download"
+    window.location.href="https://drive.google.com/file/d/1vCoAWwfoIguQHzeeyadGaWNapsln_fDv/view?usp=share_link"
+})
