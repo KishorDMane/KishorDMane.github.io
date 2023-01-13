51951040cfbd3baaf0859a7af97cc080474db0c4
KishorDMane <manekishor212@gmail.com>
2023-01-13 09:12:29 +0530

    some responcivness

--- src/resume.js
+++ src/resume.js
@@ -1,5 +1,5 @@
 let btn=document.getElementById("button")
-btn.addEventListener("click",()=>{
-    window.location.href="https://drive.google.com/u/0/uc?id=1vCoAWwfoIguQHzeeyadGaWNapsln_fDv&export=download"
+btn.addEventListener("click",async()=>{
+    window.open("https://drive.google.com/u/0/uc?id=1vCoAWwfoIguQHzeeyadGaWNapsln_fDv&export=download");
     window.location.href="https://drive.google.com/file/d/1vCoAWwfoIguQHzeeyadGaWNapsln_fDv/view?usp=share_link"
 })
