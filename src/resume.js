12c69468cd61cd2feba69920f455b9de2122ac69
KishorDMane <manekishor212@gmail.com>
2023-01-13 19:02:49 +0530

    link of project

--- src/resume.js
+++ src/resume.js
@@ -3,3 +3,25 @@ btn.addEventListener("click",async()=>{
     window.open("https://drive.google.com/u/0/uc?id=1vCoAWwfoIguQHzeeyadGaWNapsln_fDv&export=download");
     window.location.href="https://drive.google.com/file/d/1vCoAWwfoIguQHzeeyadGaWNapsln_fDv/view?usp=share_link"
 })
+
+
+
+document.getElementById("OverStock_git").addEventListener("click",()=>{
+    window.open("https://github.com/rohithanss/OverStock-Clone")
+})
+document.getElementById("OverStock_deployed").addEventListener("click",()=>{
+    window.open("https://extraordinary-cheesecake-31dd2b.netlify.app")
+})
+document.getElementById("Lifestyle_git").addEventListener("click",()=>{
+    window.open("https://github.com/Rajendra3049/Lifestylestore.com-Clone")
+})
+
+document.getElementById("Lifestyle_deployed").addEventListener("click",()=>{
+    window.open("https://lifestylestore-com-clone.vercel.app")
+})
+document.getElementById("jio_git").addEventListener("click",()=>{
+    window.open("https://github.com/KishorDMane/stitch-5509")
+})
+document.getElementById("jio_deployse").addEventListener("click",()=>{
+    window.open("https://strong-alfajores-c5eb1f.netlify.app")
+})
